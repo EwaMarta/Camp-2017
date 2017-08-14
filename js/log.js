1009698f289e96b3def649e9e7e12da56ab0e457
Piotr Kabaciski <piotr.kabacinski@gmail.com>
2017-08-14 09:33:04 +0200

    code review

--- js/log.js
+++ js/log.js
@@ -1,3 +1,5 @@
+// Brakuje zapytania do backendu :(
+
 (function(){
   const button = document.getElementsByClassName("button-log")[0];
   const input  = document.getElementsByClassName("add")[0];
@@ -8,12 +10,18 @@
     event.preventDefault();
     console.log( input.value);
 
+    /* Alternatywnie:
+      if (!input.value.length)
+      if (input.value.length === 0)
+      if (input.value.length === false)
+    */
     if (input.value.length<1){
       alert('Nie wypełniono formularza!');
       input.style.border = '2px #c00 solid';
     } else {
       window.location.href='project2.html';
-    }
+    } 
+      // Dlaczego ten return? :)
       return true;
 
   } );
