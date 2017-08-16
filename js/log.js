5fbad03493a6037ad1a7a788e5b03251baf577aa
EwaMarta <ewa.ordanik@gmail.com>
2017-08-16 19:53:30 +0200

    dashboard-nav - pierwsze kroki

--- js/log.js
+++ js/log.js
@@ -1,20 +1,59 @@
+const button = document.getElementsByClassName("submit-button")[0];
+const input  = document.getElementsByClassName("password-input")[0];
+var blockError = document.querySelector("form .error");
+const eyeButton = document.querySelectorAll("button")[2];
+
+
 (function(){
-  const button = document.getElementsByClassName("button-log")[0];
-  const input  = document.getElementsByClassName("add")[0];
-  
-  console.log(button);
     console.log(button, input);
     button.addEventListener("click", event => {
-    event.preventDefault();
-    console.log( input.value);
+      event.preventDefault();
+      console.log(input.value); });
+    button.addEventListener("click", function(event) {
+      event.preventDefault();
+      checkPassword(); });
+    eyeButton.addEventListener("click", function(event) {
+       event.preventDefault();
+       showPassword();
+    });
 
-    if (input.value.length<1){
-      alert('Nie wypełniono formularza!');
-      input.style.border = '2px #c00 solid';
-    } else {
-      window.location.href='project2.html';
-    }
-      return true;
 
-  } );
 })();
+ 
+
+function checkPassword() {
+    if (input.value.length>0) {
+      //window.location.href='project2.html';
+      //window.open('project2.html', '_blank');
+      checkResponse();
+   } else {
+      //alert('Nie wypełniono formularza!');
+      input.style.border = '2px solid #c00';
+    }
+}
+function checkResponse(){
+  $.ajax({
+      type: "post",
+      data: {
+        login: "efi",
+        password: input.value
+      },
+      url: "https://efigence-camp.herokuapp.com/api/login",
+      error: function(response) {
+        console.log(response.responseText);
+        blockError.style.visibility = "visible";
+        blockError.innerText = response.responseJSON.message;
+      },
+      success: function(response) {
+        console.log("succes", response);
+
+        input.style.border = "2px solid #0f0";
+        blockError.style.visibility = "hidden";
+      }
+  });
+}
+
+function showPassword() {
+    input.type === 'password' ? input.type = 'text' : input.type = 'password';
+}
+
